b99d5125d00725b854b672c9fd9cf3274c93a3ab
MbiluM <mabilu@usf.edu>
2025-02-13 14:36:36 -0500

    Task 6

--- cc_example.js
+++ cc_example.js
@@ -14,3 +14,23 @@ some_order = findOrders(findOrders, 101)
 console.log(```${some_order.customer}${some_order.id}$${some_order.total}```);
 
 // Task 6
+let inventory = {
+    iteams: [],
+    addIteam(name, quantity) {
+        this.iteams.push({name, quantity});
+    },
+    removeLatestIteam() {
+        this.iteams.pop();
+    },
+    removeFirstIteam() {
+        this.iteams.shift();
+    }
+};
+inventory.addIteam("Monitor", 5);
+inventory.addIteam("keyboard", 0);
+inventory.addIteam("Mouse", 3);
+console.log(inventory.iteams);
+inventory.removeLatestIteam();
+console.log(inventory.iteams);
+inventory.removeFirstIteam();
+console.log(inventory.iteams);
